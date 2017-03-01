27ec0e5eb7d077d2d3051a95a067e94cbc61e8dc
Kamil Piotrowicz <Kamil Piotrowicz@redingo>
2017-03-01 13:19:53 +0100

    Podstawowy szablon

---
+++ app/code/local/KPiotrowicz/OffEmptyConfProduct/Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *  Author: Kamil Piotrowicz
+ *  Date: 01.03.2017
+ *  Website: www.kamilpiotrowicz.pl
+ *  Location: Warsaw, Poland
+ *  
+ *  Plugin: OffEmptyConfProduct 1.0
+ *  Plugin description: Turning off the empty configurable products
+ */
+
+class KPiotrowicz_OffEmptyConfProduct_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    //this is empty :(
+}
---
+++ app/code/local/KPiotrowicz/OffEmptyConfProduct/Model/Cron.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ *  Author: Kamil Piotrowicz
+ *  Date: 01.03.2017
+ *  Website: www.kamilpiotrowicz.pl
+ *  Location: Warsaw, Poland
+ *  
+ *  Plugin: OffEmptyConfProduct 1.0
+ *  Plugin description: Turning off the empty configurable products
+ */
+
+class KPiotrowicz_OffEmptyConfProduct_Model_Cron
+{
+    public function offconfigurable()
+    {
+        //do something
+    }
+}
